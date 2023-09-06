ffc027c1fb30715809ebe208746ace9aa190845f
Nobody <nobody@nowhere.net>
2023-09-06 09:37:06 -0700

    add cust port setting

--- test_gcs.py
+++ test_gcs.py
@@ -3,6 +3,7 @@ import ssl
 import os
 
 BUCKET_NAME = "public-j5-org"
+GCS_PORT = 443
 
 try:
     host = BUCKET_NAME + ".storage.googleapis.com"
@@ -10,10 +11,11 @@ try:
         proxy_host, proxy_port = https_proxy[7:].split(":")
         print("Proxy detected: {}:{}".format(proxy_host, proxy_port))
         conn = http.client.HTTPSConnection(proxy_host, port=proxy_port, timeout=1)
-        conn.set_tunnel(host)
+        conn.set_tunnel(host, port=GCS_PORT)
     else:
         ssl_context = ssl.create_default_context()
-        conn = http.client.HTTPSConnection(host, port=443, timeout=1, context=ssl_context)
+        conn = http.client.HTTPSConnection(host, port=GCS_PORT, timeout=1, context=ssl_context)
+    print("Network Connection to https://{}:{} successful!".format(host, GCS_PORT))
     conn.request(method="GET", url="/", headers={'User-agent': "Python http.client"})
     response = conn.getresponse()
     print("HTTP status code:", response.status)
